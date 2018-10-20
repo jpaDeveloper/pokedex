582049f6f5d5c66d2e9b416bc5f2e7f121c1f9c0
jpedro12 <joao.sjc1210@gmail.com>
2018-10-20 20:51:23 -0300

    add lista de pokemons

--- pokedex/src/app/viows/list/list.component.ts
+++ pokedex/src/app/viows/list/list.component.ts
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+  pokemonList = [
+	{name: 'Bulbassaur', number: 1},
+	{name: 'Charmandar', number: 4},
+	{name: 'Squirtle', number: 7},
+	{name: 'Pikachu', number: 25}
+  ];
+
+
 
   constructor() { }
 
