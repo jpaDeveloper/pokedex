6b349667e727e8a9d2e3b291d948af1c021a7df6
jpedro12 <joao.sjc1210@gmail.com>
2018-11-06 20:16:13 -0200

    pokemon api

--- pokedex/src/app/app.module.ts
+++ pokedex/src/app/app.module.ts
@@ -1,6 +1,8 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 import { ListComponent } from './viows/list/list.component';
@@ -12,7 +14,8 @@ import { ListComponent } from './viows/list/list.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
